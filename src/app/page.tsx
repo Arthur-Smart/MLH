fa76ee146e19ca0cf62c8e050e7e4f4fa71c9840
Arthur-Smart <mwangiarthur777@gmail.com>
2024-03-06 00:50:53 -0500

    text align

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center flex flex-col">
-        <p className="text-2xl">Our Landing page is coming soon 😊?</p>
-        <p>Check the Activity page using the button below</p>
+    <div className="w-screen h-screen flex items-center justify-center flex flex-col px-4">
+        <p className="text-2xl text-center">Our Landing page is coming soon 😊?</p>
+        <p className="text-center">Check the Activity page using the button below</p>
         <Link href="/registration" className="py-2 px-3 rounded bg-[#AAA2C4] text-[#2C2C74]">Activity Page</Link>
     </div>
   )
